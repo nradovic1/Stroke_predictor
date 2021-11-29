4a0f893c3420627b5d13c102af85b23a73b5b7fc
Novak Radovic <novak_radovic@yahoo.com>
2021-11-29 16:17:56 -0500

    Model Ready

--- model.py
+++ model.py
@@ -5,15 +5,16 @@ from sklearn.model_selection import train_test_split
 from imblearn.ensemble import BalancedRandomForestClassifier
 import pickle
 
-df = pd.read_csv('model_clean_data.csv')
+df = pd.read_csv('Resources/model_clean_data.csv')
 df = df.sample(frac=1)
 
 y = df['stroke']
 X = df.drop(['stroke'], axis = 1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.85, random_state=12)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.85, random_state=12)
 
-clf = BalancedRandomForestClassifier(max_depth=2, random_state=12)
-clf.fit(X_train, y_train) 
+clf = BalancedRandomForestClassifier(max_depth=6, random_state=12)
+clf.fit(X, y) 
 
 pickle.dump(clf, open('model.pkl','wb'))
+print('Model Pickled')
