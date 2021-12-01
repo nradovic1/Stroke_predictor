29d3b1f267f2969755fb36106497b12f30d7e682
Novak Radovic <novak_radovic@yahoo.com>
2021-12-01 12:04:49 -0500

    pre-heroku-sql

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import numpy as np
 import os
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, jsonify
 from flask_pymongo import PyMongo
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 import pickle
 
@@ -35,8 +37,11 @@ def predict():
 
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    print(final_features)
     prediction = model.predict(final_features)
+
+    print(final_features)
+    print(prediction)
+    
 
     # output = round(prediction[0], 2)
     if prediction==0:
@@ -49,11 +54,21 @@ def predict():
                               )
 @app.route('/data')
 def data():
+    # Below is the code for connecting to postgresql locally. 
+    # connection_string = "postgres:postgres@localhost:5432/stroke_db"
+    # engine = create_engine(f'postgresql://{connection_string}')
 
-    results = db.session.query().all
+    # results = engine.execute('SELECT * FROM stroke_data').fetchall()
+   
+    # j_results = jsonify({'result': [dict(row) for row in results]})
 
-    return render_template('data.html',
-                                data_text = results)
+
+    return render_template('data.html') 
+
+@app.route('/explore')
+def explore():
+
+    return render_template('DataExplore.html')
 
 
 if __name__ == "__main__":
