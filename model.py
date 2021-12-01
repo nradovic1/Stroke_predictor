7cdb831f1678dde47e5711df1a9f5c05bc0d20ce
Novak Radovic <novak_radovic@yahoo.com>
2021-12-01 10:35:18 -0500

    Model Clean

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
-import numpy as np
+
 import pandas as pd
 from pathlib import Path
-from sklearn.model_selection import train_test_split
-from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from imblearn.combine import SMOTEENN
+
+#from sklearn.model_selection import train_test_split
 import pickle
 
 df = pd.read_csv('Resources/model_clean_data.csv')
@@ -13,8 +15,16 @@ X = df.drop(['stroke'], axis = 1)
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.85, random_state=12)
 
-clf = BalancedRandomForestClassifier(max_depth=6, random_state=12)
-clf.fit(X, y) 
+smoteenn = SMOTEENN(random_state=12)
+X_smoteenn, y_smoteenn = smoteenn.fit_resample(X, y)
 
-pickle.dump(clf, open('model.pkl','wb'))
+knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
+model = knn.fit(X_smoteenn, y_smoteenn)
+
+
+
+
+
+
+pickle.dump(model, open('model.pkl','wb'))
 print('Model Pickled')
